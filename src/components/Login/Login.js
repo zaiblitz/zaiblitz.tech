781d101018acac690f2119247cbeb53e52e8bc7c
Merwin Dale <merwindale.domingo@gmail.com>
2020-11-04 13:34:16 +0800

    Update Link

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,7 +5,8 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
+// import Link from '@material-ui/core/Link';\
+
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -13,15 +14,16 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
+import { Link } from 'react-router-dom';
 import { useHistory } from "react-router";
 import axios from 'axios';
 import { API_URL } from '../../contexts/API';
 
-function Copyright() {
+function Copyright(classes) {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" to="/signin" className={classes.link}>
         zaiblitz.tech
       </Link>{' '}
       {new Date().getFullYear()}
@@ -48,6 +50,13 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
+  link: {
+      textDecoration: 'none',
+      color: '#3f51b5',
+      "&:hover, &:focus": {
+          textDecoration: 'underline'
+      },
+  }
 }));
 
 export default function SignIn() {
@@ -130,12 +139,12 @@ export default function SignIn() {
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link href="#" variant="body2">
+              <Link href="#" variant="body2" className={classes.link}>
                 Forgot password?
               </Link>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link to="/signup" variant="body2" className={classes.link}>
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
@@ -143,7 +152,7 @@ export default function SignIn() {
         </form>
       </div>
       <Box mt={8}>
-        <Copyright />
+        <Copyright classes={classes}/>
       </Box>
     </Container>
   );
